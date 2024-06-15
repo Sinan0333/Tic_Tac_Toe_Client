5f2b5813c6b3a57d747fdc39b5b785c5414ac8a5
Mohammed Sinan Mp <sinan.m.p333@gmail.com>
2024-06-15 11:08:30 +0530

    loged env

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import {Provider} from "react-redux"
 import store,{persistor} from './store/store.js'
 import { PersistGate } from 'redux-persist/integration/react'
 import { GoogleOAuthProvider } from '@react-oauth/google';
-
+console.log(import.meta.env)
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
